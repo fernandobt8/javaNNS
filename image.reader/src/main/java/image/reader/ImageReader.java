4dbabcdb61366ed246f7782e9caa2ad7af7568c3
fernando burigo <fernandobt8@gmail.com>
2015-06-02 23:00:20 -0300

    cinza normalizado

--- image.reader/src/main/java/image/reader/ImageReader.java
+++ image.reader/src/main/java/image/reader/ImageReader.java
@@ -13,20 +13,21 @@ import javax.imageio.ImageIO;
 public class ImageReader {
 
 	public static void main(String[] args) throws IOException {
-		File input = new File("/home/fernandobt8/github/javaNNS.images/image.reader/src/main/resources/9.png");
+		File input = new File("/home/fernandobt8/github/javaNNS.images/image.reader/src/main/resources/0.png");
 		System.out.println(input.getAbsolutePath());
 		BufferedImage read = ImageIO.read(input);
-		List<Integer> colors = new LinkedList<>();
+		List<Double> colors = new LinkedList<>();
 		for (int i = 0; i < read.getHeight(); i++) {
 			for (int j = 0; j < read.getWidth(); j++) {
 				int rgb = read.getRGB(j, i);
 				Color color = new Color(rgb);
-				colors.add((color.getBlue() + color.getGreen() + color.getRed()) / 3);
+				colors.add((color.getBlue() + color.getGreen() + color.getRed()) / 3 / 255.0);
 			}
 		}
 		String text = "";
 		NumberFormat instance = NumberFormat.getInstance();
-		instance.setMinimumIntegerDigits(3);
+		instance.setMaximumFractionDigits(3);
+		instance.setMinimumFractionDigits(3);
 		for (int i = 0; i < colors.size(); i++) {
 			if (i % 10 == 0) {
 				text += "\n";
